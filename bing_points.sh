858c3c86eca4358d9d189f40db9974597946fbc7
Venom120 <yatharthjain120@gmail.com>
2025-06-30 11:10:30 +0530

    Update desktop entry to execute script and enhance bash script for environment setup

--- bing_points.sh
+++ bing_points.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This script sets up the environment and runs the Bing Points automation script.
 cd /mnt/2050F97550F95250/Github/Bing-Points
-.venv/bin/python main.py
+source .venv/bin/activate
+python main.py
