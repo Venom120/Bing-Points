bf16701cdfb50b87b52afaab389019f07201d141
Venom120 <yatharthjain120@gmail.com>
2025-08-15 14:19:25 +0530

    updated windows bat file

--- bing_points.bat
+++ bing_points.bat
@@ -4,16 +4,5 @@ REM This script is used to run the Bing Points automation script.
 REM Get the directory of the batch file
 cd /d "%~dp0"
 
-REM Install Python dependencies
-pip show selenium >nul 2>&1
-if errorlevel 1 (
-    pip install selenium
-)
-
-pip show webdriver-manager >nul 2>&1
-if errorlevel 1 (
-    pip install webdriver-manager
-)
-
 REM Run the main script
 python main.py
